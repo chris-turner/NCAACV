5a55359a550b486b82eb27ddfa4c3aaf45773b69
Christopher Turner <cturner87@gatech.edu>
2025-12-24 12:30:29 -0500

    tracking

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from utils import read_video, save_video
+from trackers import Tracker
 
 def main():
     video_frames = read_video('videos\AmariLatimer.mp4')
 
-    save_video(video_frames, 'videos\output_video.mp4')
+    tracker = Tracker('models/best.pt')
+
+    tracks = tracker.get_object_tracks(video_frames)
+
+    #save_video(video_frames, 'videos\output_video.mp4')
 
 if __name__ == "__main__":
     main()
--- yolo_inference.py
+++ yolo_inference.py
@@ -5,7 +5,7 @@ model = YOLO('models/best.pt')
 #results = model.predict('videos/08fd33_4.mp4', save=True)
 results = model.predict('videos/AmariLatimer.mp4', save=True)
 
-print(results[0])
-print('-----------------------')
-for box in results[0].boxes:
-    print(box)
+#print(results[0])
+#print('-----------------------')
+#for box in results[0].boxes:
+#   print(box)
